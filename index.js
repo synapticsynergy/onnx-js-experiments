ecb72fbe2900e9740b0cd953b8fa2150894c5101
Blake Danson <blakedanson@gmail.com>
2020-03-29 13:47:56 -0700

    Add inference for pdxTrees model

--- index.js
+++ index.js
@@ -3,22 +3,33 @@ const sharp = require('sharp');
 const axios = require('axios');
 const ndarray = require('ndarray');
 const ops = require('ndarray-ops');
+const fs = require('fs');
 
 let hrstart = process.hrtime()
 
 async function main(url){
-  let hrend = process.hrtime(hrstart)
   // image preprocessing
   const input = (await axios({ url: url, responseType: "arraybuffer" })).data;
   const { data, info } = await sharp(input)
+    .ensureAlpha()
     .resize(224, 224)
     .raw()
     .toBuffer({ resolveWithObject: true });
-  // const incomingData = new Uint8Array(data);
-  // const float32 = new Float32Array(data);
-  // const float32 = Float32Array.from(incomingData);
 
-  const dataFromImage = ndarray(new Float32Array(data), [info.width, info.height, 4]);
+  const imageData = preprocess(data,224,224)
+  const result = await predict(imageData,224,224);
+
+  return result;
+}
+
+function argMax(array) {
+  return Array.from(array).map((x, i) => [x, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
+
+
+function preprocess(data, width, height) {
+  const dataFromImage = ndarray(new Float32Array(data), [width, height, 4]);
+  // Normalize 0-255 to [0, 1]
   ops.divseq(dataFromImage, 255);
   ops.subseq(dataFromImage.pick(0, null, null), 0.485);
   ops.divseq(dataFromImage.pick(0, null, null), 0.229);
@@ -26,28 +37,34 @@ async function main(url){
   ops.divseq(dataFromImage.pick(1, null, null), 0.224);
   ops.subseq(dataFromImage.pick(2, null, null), 0.406);
   ops.divseq(dataFromImage.pick(2, null, null), 0.225);
-  
-  //  [224,224,4] => [1,3,224,224]
-  const dataProcessed = ndarray(new Float32Array(info.width * info.height * 3), [1, 3, info.height, info.width]);
-  // ops.assign(dataProcessed.pick(0, 0, null, null), dataFromImage.pick(null, null, 0));
-  // ops.assign(dataProcessed.pick(0, 1, null, null), dataFromImage.pick(null, null, 1));
-  // ops.assign(dataProcessed.pick(0, 2, null, null), dataFromImage.pick(null, null, 2));
 
-  const tensor = new onnx.Tensor(dataProcessed.data, 'float32', [1, 3, info.width, info.height]);
-  // const tensor = new onnx.Tensor(float32, 'float32', [1, 3, info.width, info.height]);
-  // console.log(tensor)
+  // Realign imageData from [224*224*4] to the correct dimension [1*3*224*224].
+  const dataProcessed = ndarray(new Float32Array(width * height * 3), [1, 3, height, width]);
+  ops.assign(dataProcessed.pick(0, 0, null, null), dataFromImage.pick(null, null, 0));
+  ops.assign(dataProcessed.pick(0, 1, null, null), dataFromImage.pick(null, null, 1));
+  ops.assign(dataProcessed.pick(0, 2, null, null), dataFromImage.pick(null, null, 2));
 
-  // inference
-  const session = new onnx.InferenceSession();
-  const model = "./bestmodel.onnx";
-  await session.loadModel(model);
-  const outputMap = await session.run([tensor]);
-  console.log(outputMap.values())
-  console.log('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
-  console.log(hrend[1],' nano seconds')
-
+  return dataProcessed.data;
 }
 
-main('https://b1.pngbarn.com/png/412/963/tree-16-palm-tree-png-clip-art-thumbnail.png')
-// main('http://storage.needpix.com/rsynced_images/tree-1439369_1280.jpg')
+
+async function predict(preprocessedData, modelWidth=224, modelHeight=224){
+  let hrend = process.hrtime(hrstart)
+  const modelName = 'pdxTrees';
+  const model = `./models/${modelName}.onnx`;
+  const classLabels = JSON.parse(fs.readFileSync(`./labels/${modelName}.json`,'utf-8'))["classLabels"];
+
+  const session = new onnx.InferenceSession();
+  await session.loadModel(model);
+  const inputTensor = new onnx.Tensor(preprocessedData, 'float32', [1, 3, modelWidth, modelHeight]);
+  const classProbabilities = await session.run([inputTensor]);
+  const outputData = classProbabilities.values().next().value.data;
+  console.log('Execution time (hr): %ds %dms', hrend[0], hrend[1] / 1000000)
+  console.log(hrend[1],' nano seconds')
+  return classLabels[argMax(outputData)];
+}
+
+main('https://i.pinimg.com/originals/fb/13/dd/fb13dd2ab6cc760b3ac5ca15b9de11a6.jpg').then((result)=>{
+  console.log(result)
+});
 
